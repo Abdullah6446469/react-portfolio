11ab3b1ee5b33157cefc96f7098f203a465ba865
Abdullah <abdulojonsattorov@gmail.com>
2023-08-27 19:04:01 +0500

    first change

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,147 @@
+import './index.scss'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-a.png' 
+import LogoSubtitle from '../../assets/images/logo-y-abdullah.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+  faLinkedin,
+  faGithub,
+  faYoutube, 
+  faTelegram, 
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link 
+        className="logo"
+        to="/"
+        onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon' />
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/abdullah-s-97ba17285"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Abdullah6446469"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://youtube.com/@ProAIdeveloper?si=7HoP7h_b6vJAnAO1"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li> 
+        <li>
+          <a
+            href="https://t.me/profprogrammers"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faTelegram}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://join.skype.com/tryLsS58a8MA" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
+    </div>
+  )
+}
+
+export default Sidebar
